35c2a3a455a9bbc9eb4234ec05cffb114b095cc0
Salvador Rioboo Naranjo <salrio74@gmail.com>
2024-12-07 18:31:35 +0100

    chore: corrected norminette

--- 00-libft/test/ft_test_base.c
+++ 00-libft/test/ft_test_base.c
@@ -5,7 +5,7 @@
 #include <assert.h>
 #include <stdlib.h>
 
-static void	fun_start(const char* method_name)
+static void	fun_start(const char *method_name)
 {
 	printf("\n==== TEST: %s ====\n", method_name);
 }
@@ -15,9 +15,9 @@ static void	fun_end(const char *method_name)
 	printf("\n==== OK: %s ====\n", method_name);
 }
 
-void test_ft_isalpha(void)
+void	test_ft_isalpha(void)
 {
-	const char *method_name = "test_ft_isalpha";
+	const char	*method_name = "test_ft_isalpha";
 
 	fun_start(method_name);
 	printf("--- orig ---\n");
@@ -25,15 +25,15 @@ void test_ft_isalpha(void)
 	fun_end(method_name);
 }
 
-void test_ft_atoi(void)
+void	test_ft_atoi(void)
 {
-	const char *method_name = "test_ft_atoi";
-	const char  *nptr = "--45623a";
+	const char	*method_name = "test_ft_atoi";
+	const char	*nptr = "--45623a";
 
 	fun_start(method_name);
 	printf("--- orig ---\n");
 	printf("%d\n", atoi(nptr));
 	printf("--- new ---\n");
-	// printf("%d\n", ft_atoi(nptr));
+	// TODO - printf("%d\n", ft_atoi(nptr));
 	fun_end(method_name);
 }
--- 00-libft/test/ft_test_mem.c
+++ 00-libft/test/ft_test_mem.c
@@ -5,35 +5,40 @@
 #include <assert.h>
 #include <stdlib.h>
 
-static void fun_start(const char *method_name)
+static void	fun_start(const char *method_name)
 {
 	printf("\n==== TEST: %s ====\n", method_name);
 }
 
-static void fun_end(const char *method_name)
+static void	fun_end(const char *method_name)
 {
 	printf("\n==== OK: %s ====\n", method_name);
 }
 
-int test_ft_memset(void)
+int	test_ft_memset(void)
 {
-	const char *method_name = "test_ft_memset";
-	size_t len = 100;
-	void *ptr = malloc(len);
+	const char	*method_name = "test_ft_memset";
+	size_t		len = 100;
+	void		*ptr = malloc(len);
+	size_t		i;
+
 	if (ptr == NULL)
 	{
 		fprintf(stderr, "Not enough memory\n");
-		return 0; // or whatever, do not continue
+		return (0); // or whatever, do not continue
 	}
 	fun_start(method_name);
 	printf("--- orig ---\n");
 	memset(ptr, 0, len);
 	char *base = ptr;
-	for (size_t i = 0; i < len; i++)
+	i = 0;
+	while (base[i])
+	{
 		assert(base[i] == 0);
+		i++;
+	};
 	free(ptr);
 	printf("--- new ---\n");
 	fun_end(method_name);
-
-	return 0;
+	return (0);
 }
