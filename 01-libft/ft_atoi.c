c1633fcd8c28a86b9c5a02c7ef636c88efd6fb11
Salvador Rioboo Naranjo <salrio74@gmail.com>
2024-12-08 10:20:56 +0100

    fix: changed ft_atoi.c

--- 01-libft/ft_atoi.c
+++ 01-libft/ft_atoi.c
@@ -10,29 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-// TODO - completar
-#include <stdio.h>
-
-int	ft_atoi(const char *nptr)
+int	ft_atoi(const char *cton)
 {
 	int	result;
 	int	sign;
-	// int exp = 1;
-	int i = 0;
+	int	i;
 
 	result = 0;
 	sign = 1;
-	while (nptr[i] == '-')
+	i = 0;
+	while (cton[i] == '-' || cton[i] == '+')
 	{
-		sign = -1;
+		if (cton[i] == '-')
+			sign *= -1;
 		i++;
 	}
-
-	while (nptr[i] >= '0' && nptr[i] <= '9')
+	while (cton[i] >= '0' && cton[i] <= '9')
 	{
-		printf("%c\n", nptr[i]);
+		result = result * 10 + (cton[i] - '0');
 		i++;
 	}
-
 	return (sign * result);
 }
--- 01-libft/test/ft_test_base.c
+++ 01-libft/test/ft_test_base.c
@@ -4,6 +4,7 @@
 #include "../libft.h"
 #include <assert.h>
 #include <stdlib.h>
+#include <limits.h>
 
 static void	fun_start(const char *method_name)
 {
@@ -28,12 +29,37 @@ void	test_ft_isalpha(void)
 void	test_ft_atoi(void)
 {
 	const char	*method_name = "test_ft_atoi";
-	const char	*nptr = "-45623a";
+	const char	*nptr = "-4562a4";
+	const char	*nptrb = "+567";
+	const char	*nptrc = "  +-567";
+	const char	*nptrd = "2147483647";
+	const char	*nptre = "-2147483647";
 
 	fun_start(method_name);
+	printf("--- test: '%s'\n", nptr);
 	printf("--- orig ---\n");
 	printf("%d\n", atoi(nptr));
 	printf("--- new ---\n");
 	printf("%d\n", ft_atoi(nptr));
+	printf("--- test: '%s'\n", nptrb);
+	printf("--- orig ---\n");
+	printf("%d\n", atoi(nptrb));
+	printf("--- new ---\n");
+	printf("%d\n", ft_atoi(nptrb));
+	printf("--- test: '%s'\n", nptrc);
+	printf("--- orig ---\n");
+	printf("%d\n", atoi(nptrb));
+	printf("--- new ---\n");
+	printf("%d\n", ft_atoi(nptrb));
+	printf("--- test: '%s'\n", nptrd);
+	printf("--- orig ---\n");
+	printf("%d\n", atoi(nptrd));
+	printf("--- new ---\n");
+	printf("%d\n", ft_atoi(nptrd));
+	printf("--- test: '%s'\n", nptre);
+	printf("--- orig ---\n");
+	printf("%d\n", atoi(nptre));
+	printf("--- new ---\n");
+	printf("%d\n", ft_atoi(nptre));
 	fun_end(method_name);
 }
